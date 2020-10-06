78d7b438325f6dc2bc56947774b1f3b755efd51e
Bryan Tarpley <bptarpley@tamu.edu>
2020-10-06 15:58:21 -0500

    fixed field-level actions on corpus page (deletion; re-ordering); fixed bug in CSV data importer w/ blank spaces

--- app/plugins/csv/tasks.py
+++ app/plugins/csv/tasks.py
@@ -78,13 +78,18 @@ def import_csv_data(job_id):
                 for field_name in row.keys():
                     ct_field = ct.get_field(field_name)
                     if ct_field:
-                        if row[field_name]:
-                            setattr(content, field_name, row[field_name])
+                        if row[field_name].strip():
+                            setattr(content, field_name, row[field_name].strip())
                         else:
                             setattr(content, field_name, None)
                         needs_saving = True
 
                 if needs_saving:
-                    content.save()
+                    try:
+                        content.save()
+                    except:
+                        print("Error importing data from CSV row!")
+                        print(content.to_json())
+                        print(traceback.format_exc())
 
     job.complete(status='complete')
