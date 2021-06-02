8775ba9a2d2b0ea8920ca22e51c7f90f161a5c6a
Bryan Tarpley <bptarpley@tamu.edu>
2021-06-02 11:44:51 -0500

    NVS: playviewer changes and site_urls conf

--- app/plugins/nvs/site_urls.py
+++ app/plugins/nvs/site_urls.py
@@ -3,10 +3,11 @@ from . import views as nvs_views
 
 
 urlpatterns = [
-    path('', nvs_views.splash),
-    path('play/<str:play_prefix>/', nvs_views.playviewer),
+    path('', nvs_views.home),
+    path('edition/<str:play_prefix>/', nvs_views.playviewer),
     path('minimap/<str:play_prefix>/', nvs_views.play_minimap),
-    path('paratext/<str:play_prefix>/<str:section>/', nvs_views.paratext),
+    path('appendix/<str:play_prefix>/', nvs_views.paratext, {'section': 'Appendix'}),
+    path('front/<str:play_prefix>/', nvs_views.paratext, {'section': 'Front Matter'}),
     path('witnessmeter/<str:witness_flags>/<str:height>/<str:width>/<str:inactive_color_hex>/<str:label_buffer>/', nvs_views.witness_meter),
     path('search/<str:play_prefix>/', nvs_views.api_search),
     path('search/', nvs_views.api_search),
