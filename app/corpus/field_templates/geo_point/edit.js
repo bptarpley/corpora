15c2f062fc1bad6868b3649897cbb6e5ff5650c9
Bryan Tarpley <bptarpley@gmail.com>
2026-09-24 13:27:41 -0500

    explicitly setting a referrer policy when requesting Leaflet map tiles

--- app/corpus/field_templates/geo_point/edit.js
+++ app/corpus/field_templates/geo_point/edit.js
@@ -17,7 +17,8 @@ function renderGeoPointMap(target) {
     L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
         noWrap: true,
         maxZoom: 19,
-        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+        attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>',
+        referrerPolicy: 'strict-origin-when-cross-origin'
     }).addTo(map)
 
     if (lat_input.val() && long_input.val()) {
